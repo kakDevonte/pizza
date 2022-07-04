b75eaee387789eab5c2e4ecb861e5611d7c56585
HatyushinAS <kakdevonte@yandex.ru>
2022-07-04 11:54:01 +0300

    add change

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
-const FullPizza = () => {
+const FullPizza: React.FC = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [pizza, setPizza] = React.useState();
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: number;
+  }>();
 
   React.useEffect(() => {
     async function fetchPizza() {
@@ -23,11 +27,11 @@ const FullPizza = () => {
   }, []);
 
   if (!pizza) {
-    return 'Загрузка...';
+    return <>Загрузка...</>;
   }
   return (
     <div className="container">
-      <img src={pizza.imageUrl} />
+      <img src={pizza.imageUrl} alt="Картинка пиццы" />
       <h2>{pizza.title}</h2>
       <h4>{pizza.price} ₽</h4>
     </div>
